077016fcaa4a51d40869496f3f91d46b50d59393
razerx100 <saikatmandal8@gmail.com>
2022-01-28 09:32:25 +0600

    idk what I was thinking before

--- HLSL/src/VSColored.hlsl
+++ HLSL/src/VSColored.hlsl
@@ -3,10 +3,10 @@ struct VSOut {
     float4 position : SV_Position;
 };
 
-VSOut main(float3 position : Position) {
+VSOut main(float3 position : Position, float4 color : Color) {
     VSOut obj;
     obj.position = float4(position, 1.0f);
-    obj.color = float4(1.0f, 0.0f, 1.0f, 1.0f);
+    obj.color = color;
 
     return obj;
 }
