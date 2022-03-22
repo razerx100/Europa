c5af02320dcc2fd1574d0cf6a1d785ce6b4ca820
razerx100 <saikatmandal8@gmail.com>
2022-03-22 21:12:45 +0600

    added runtime texture changing support

--- HLSL/src/PixelShader.hlsl
+++ HLSL/src/PixelShader.hlsl
@@ -1,3 +1,11 @@
-float4 main(float4 colour : Colour) : SV_Target {
-    return colour;
+struct TextureConstants {
+    uint texIndex;
+};
+
+ConstantBuffer<TextureConstants> texConstants : register(b0);
+Texture2D g_textures[] : register(t0);
+SamplerState samplerState : register(s0);
+
+float4 main(float2 uv: UV) : SV_Target {
+    return g_textures[texConstants.texIndex].Sample(samplerState, uv);
 }
--- HLSL/src/VertexShader.hlsl
+++ HLSL/src/VertexShader.hlsl
@@ -1,12 +1,31 @@
+struct TextureInfo {
+    uint uStart;
+    uint uEnd;
+    uint uMax;
+    uint vStart;
+    uint vEnd;
+    uint vMax;
+};
+
 struct VSOut {
-    float4 colour : Colour;
+    float2 uv : UV;
     float4 position : SV_Position;
 };
 
-VSOut main(float3 position : Position, float4 colour : Colour) {
+ConstantBuffer<TextureInfo> texInfo : register(b1);
+
+float PixelToUV(uint pixelCoord, uint maxLength) {
+    return (float)((pixelCoord - 1) * 2 + 1) / (maxLength * 2);
+}
+
+VSOut main(float3 position : Position) {
     VSOut obj;
     obj.position = float4(position, 1.0f);
-    obj.colour = colour;
+
+    obj.uv = float2(
+        PixelToUV(texInfo.uStart, texInfo.uMax),
+        PixelToUV(texInfo.vStart, texInfo.vMax)
+    );
 
     return obj;
 }
