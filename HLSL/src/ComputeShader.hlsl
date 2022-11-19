970cf1d5d4dbbc859a8441776169eed032ea8f46
razerx100 <saikatmandal8@gmail.com>
2022-11-20 01:46:07 +0600

    UAV test

--- HLSL/src/ComputeShader.hlsl
+++ HLSL/src/ComputeShader.hlsl
@@ -25,11 +25,20 @@ struct IndirectCommand {
     }indexedArguments;
 };
 
+struct CullingData {
+    uint commandCount;
+};
+
 StructuredBuffer<PerModelData> b_modelData : register(t0);
-StructuredBuffer<IndirectCommand> b_indirectCommands : register(t1);
+StructuredBuffer<IndirectCommand> b_inputCommands : register(t1);
+AppendStructuredBuffer<IndirectCommand> b_outputCommands : register(u0);
 ConstantBuffer<CameraMatrices> b_camera : register(b0);
+ConstantBuffer<CullingData> cullingData : register(b1);
 
 [numthreads(threadBlockSize, 1, 1)]
 void main(uint3 groupId : SV_GroupID, uint groupIndex : SV_GroupIndex) {
     uint index = (groupId.x * threadBlockSize) + groupIndex;
+
+    if(cullingData.commandCount > index)
+        b_outputCommands.Append(b_inputCommands[index]);
 }
