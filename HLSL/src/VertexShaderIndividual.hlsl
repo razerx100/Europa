977d29a379a651ae74be3d26f27b234d98d57b28
razerx100 <saikatmandal8@gmail.com>
2024-09-22 11:55:18 +0100

    Add HLSL shader for VSIndividual

--- HLSL/src/VertexShaderIndividual.hlsl
+++ HLSL/src/VertexShaderIndividual.hlsl
@@ -1,35 +1,77 @@
-#include "VertexProcessing.hlsli"
-
-struct ModelData {
-    matrix modelMat;
-    matrix viewNormalMatrix;
-    float3 modelOffset;
-    float padding;
+struct VertexOut
+{
+	float3 viewVertexPosition : ViewPosition;
+	float3 normal             : Normal;
+	float2 uv                 : UV;
+	uint   modelIndex         : ModelIndex;
+    uint   materialIndex      : MaterialIndex;
+    float4 position           : SV_Position;
 };
 
-struct ModelInfo {
-    uint index;
+struct ModelData
+{
+    matrix modelMatrix;
+    float4 modelOffset; // materialIndex on the last component.
 };
 
-struct CameraMatrices {
+struct Vertex
+{
+    float3 position;
+    float3 normal;
+    float2 uv;
+};
+
+struct CameraMatrices
+{
     matrix view;
     matrix projection;
 };
 
-StructuredBuffer<ModelData> modelData : register(t0);
-ConstantBuffer<ModelInfo> modelInfo : register(b0);
-ConstantBuffer<CameraMatrices> camera : register(b1);
+struct ConstantData
+{
+    uint modelIndex;
+};
 
-VertexOut main(float3 position : Position, float3 normal : Normal, float2 uv : UV) {
-    const ModelData model = modelData[modelInfo.index];
+StructuredBuffer<ModelData> modelData     : register(t0);
+ConstantBuffer<ConstantData> constantData : register(b0);
+ConstantBuffer<CameraMatrices> cameraData : register(b1);
 
-    Vertex vertex;
-    vertex.position = position;
-    vertex.normal = normal;
-    vertex.uv = uv;
+VertexOut GetVertexAttributes(
+    matrix modelM, matrix viewM, matrix projectionM, matrix viewNormalM, Vertex vertex,
+	float3 modelOffset, uint modelIndex, uint materialIndex
+) {
+    matrix viewSpace          = mul(viewM, modelM);
+    float4 vertexPosition     = float4(vertex.position + modelOffset, 1.0);
+    float4 viewVertexPosition = mul(viewSpace, vertexPosition);
 
-    return GetVertexAttributes(
-        model.modelMat, camera.view, camera.projection, model.viewNormalMatrix, vertex,
-        model.modelOffset, modelInfo.index
-    );
+    VertexOut vout;
+    vout.position           = mul(projectionM, viewVertexPosition);
+    vout.viewVertexPosition = viewVertexPosition.xyz;
+    vout.normal             = mul((float3x3) viewNormalM, vertex.normal);
+    vout.uv                 = vertex.uv;
+    vout.modelIndex         = modelIndex;
+    vout.materialIndex      = materialIndex;
+
+    return vout;
+}
+
+VertexOut main(float3 position : Position, float3 normal : Normal, float2 uv : UV)
+{
+	const ModelData model = modelData[constantData.modelIndex];
+
+	Vertex vertex;
+	vertex.position = position;
+	vertex.normal   = normal;
+	vertex.uv       = uv;
+
+    float3 modelOffset      = model.modelOffset.xyz;
+    // Probably going to calculate this on the CPU later.
+    // matrix viewNormalMatrix = transpose(mul(model.modelMatrix, cameraData.view));
+    matrix viewNormalMatrix = model.modelMatrix;
+    uint materialIndex      = asuint(model.modelOffset.w);
+
+	return GetVertexAttributes(
+		model.modelMatrix, cameraData.view, cameraData.projection, viewNormalMatrix, vertex,
+		modelOffset, constantData.modelIndex, materialIndex
+	);
 }
